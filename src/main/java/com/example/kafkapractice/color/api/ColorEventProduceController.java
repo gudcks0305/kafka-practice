3ebce989ee4f4b132e57a6184def8dd1263359a6
yoo hyeong chan <84506708+gudcks0305@users.noreply.github.com>
2023-03-05 22:03:25 +0900

    feat : 카프카 topic favorite-color producer 설정

--- src/main/java/com/example/kafkapractice/color/api/ColorEventProduceController.java
+++ src/main/java/com/example/kafkapractice/color/api/ColorEventProduceController.java
@@ -1,10 +1,23 @@
 package com.example.kafkapractice.color.api;
 
+import com.example.kafkapractice.color.domain.service.FavoriteEventProduceService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
+@RequestMapping("/api")
 public class ColorEventProduceController {
+    private final FavoriteEventProduceService favoriteEventProduceService;
 
+    public ColorEventProduceController(FavoriteEventProduceService favoriteEventProduceService) {
+        this.favoriteEventProduceService = favoriteEventProduceService;
+    }
+    @GetMapping("/select-color")
+    public void selectColor(@RequestHeader("user-agent") String userAgent ,
+                            @RequestParam("user") String userName,
+                            @RequestParam("color") String colorName) {
+        log.info("userAgent: {}", userAgent);
+        favoriteEventProduceService.produceFavoriteEvent(userName, colorName, userAgent);
+    }
 }
--- src/main/java/com/example/kafkapractice/color/domain/service/FavoriteEventProduceService.java
+++ src/main/java/com/example/kafkapractice/color/domain/service/FavoriteEventProduceService.java
@@ -3,5 +3,5 @@ package com.example.kafkapractice.color.domain.service;
 import org.springframework.stereotype.Service;
 
 public interface FavoriteEventProduceService {
-
+    void produceFavoriteEvent(String userName, String colorName,String userAgent);
 }
